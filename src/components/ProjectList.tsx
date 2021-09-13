2c18ca8b74d42effcbd54820dc448eaf9c5aca2c
Gonzalo Aguado Torres <gonagutor@gmail.com>
2021-09-13 15:37:21 +0200

    Everything but DnDmin and navbar glitches

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -17,39 +17,14 @@ const ProjectList = (props: ProjectListProps) => {
 
   for (let i = 0; i < Data.projects.length; i += 1) {
     projects.push(
-      <section
-        key={i}
-        style={{
-          display: 'flex',
-          flexDirection: ((i % 2 === 0)) ? 'row' : 'row-reverse',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-          padding: '5vw',
-          flexBasis: '40%',
-          color: ((i % 2 === 0)) ? 'var(--black-text)' : 'var(--white-text)',
-          background: ((i % 2 === 0)) ? 'var(--white)' : 'var(--black)',
-          backgroundImage: 'var(--noise)',
-        }}
-      >
-        <div style={{
-          flexGrow: 6, display: 'flex', flexDirection: 'column',
-        }}
-        >
-          <h1 style={{ fontFamily: '"Vaulto", sans-serif', fontWeight: 'bold', fontSize: '2rem' }}>{Data.projects[i].title}</h1>
-          <div style={{ fontFamily: '"Open Sans", sans-serif', fontSize: '1.2rem' }}>{sliceNewLines(Data.projects[i].description)}</div>
+      <section key={i} className={`projectContainer ${((i % 2 === 0)) ? 'even' : 'odd'}`}>
+        <div className="projectTextContainer">
+          <h1 className="projectTitle">{Data.projects[i].title}</h1>
+          <div className="projectDescriptionContainer">
+            {sliceNewLines(Data.projects[i].description)}
+          </div>
         </div>
-        <img
-          style={{
-            flexGrow: 4,
-            maxWidth: '480px',
-            maxHeight: '270px',
-            padding: '5vw',
-            objectFit: 'contain',
-          }}
-          src={Data.projects[i].image}
-          alt={Data.projects[i].title}
-        />
+        <img className="projectImage" src={Data.projects[i].image} alt={Data.projects[i].title} />
       </section>,
     );
   }
